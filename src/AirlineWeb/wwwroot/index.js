33be279667d8005a462e68d91cc0a76459bcfdd6
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-04 19:50:13 +0200

    Add Static Index Page
    Created a simple static index.html page with a little bit of JS to register new webhooks via our API

---
+++ src/AirlineWeb/wwwroot/index.js
@@ -0,0 +1,42 @@
+﻿let registerButton = document.querySelector("#register");
+let webhookURI = document.querySelector("#webhook");
+let webhookType = document.querySelector("#webhookType");
+let successBox = document.querySelector("#alertSuccess");
+let dangerBox = document.querySelector("#alertDanger");
+let dangerMessage = document.querySelector("#dangerMessage");
+let successMessage = document.querySelector("#successMessage");
+
+successBox.style.display = 'none';
+dangerBox.style.display = 'none';
+
+registerButton.onclick = () => {
+    successBox.style.display = 'none';
+    dangerBox.style.display = 'none';
+
+    if (webhookURI.value === "") {
+        dangerMessage.innerHTML = "Please enter a URI";
+        dangerBox.style.display = 'block';
+    } else {
+        (async () => {
+            const rawResponse = await fetch('https://localhost:5001/api/v1/WebhookSubscription',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(
+                        {
+                            webhookUri: webhookURI.value,
+                            webhookType: webhookType.value,
+                        }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+            const content = await rawResponse.json();
+
+            successMessage.innerHTML =
+                "Webhook Registered! Please use secret: " + content.secret + " to validate inbound request";
+
+            successBox.style.display = 'block';
+        })();
+    }
+}
